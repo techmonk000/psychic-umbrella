eedf6a43178571f28f1326f60f1ffa3cc1768bdf
Swarnavo44 <swarnavomukherjee03@gmail.com>
2023-08-20 19:43:29 +0530

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from random import randint
-for i in range(1, 36):
+for i in range(1, 365):
     for j in range(0, randint(1, 10)):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
